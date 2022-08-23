04bfd979e9657c3a88a1eac869e2921e0445e954
Иван Шаронов <sharonov.ivan.web@gmail.com>
2022-08-23 17:44:23 +0400

    поиск по логину поиск по критерию

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -3,6 +3,7 @@
 namespace App\Manager;
 
 use App\Entity\User;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\EntityManagerInterface;
 
 class UserManager
@@ -44,5 +45,29 @@ class UserManager
         $author->addFollower($follower);
         $follower->addAuthor($author);
         $this->entityManager->flush();
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findUsersByLogin(string $name): array
+    {
+        $repository = $this->entityManager->getRepository(User::class);
+
+        return $repository->findBy(['login' => $name]);
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findUsersByCriteria(string $login): array
+    {
+        $criteria = Criteria::create();
+        /** @noinspection NullPointerExceptionInspection */
+        $criteria->andWhere(Criteria::expr()->eq('login', $login));
+        /** @var EntityRepository $repository */
+        $repository = $this->entityManager->getRepository(User::class);
+
+        return $repository->matching($criteria)->toArray();
     }
 }
